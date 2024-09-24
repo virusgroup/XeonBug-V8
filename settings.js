b67fd894b6ef17046f870bb45f108a6c4f13f222
sarwarali32 <kingjamot42@gmail.com>
2024-09-24 16:50:23 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "923300795089"
+global.ownername = "Sarwar Ali"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '923300795089'  //creator number
+global.ownername = 'Sarwar Ali' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
